d363b0789d14926baf05191f988f18046ed24148
ben-freke <benjaminfreke@gmail.com>
2018-06-18 21:11:47 +0100

    Functioning with FollowMee

---
+++ app/models/config.php
@@ -0,0 +1,21 @@
+<?php
+
+
+class config extends Phalcon\Mvc\Model
+{
+    //
+    public $id;
+    public $variable;
+    public $value;
+
+    public function initialize()
+    {
+    }
+    public function beforeSave()
+    {
+    }
+    public function afterFetch()
+    {
+    }
+
+}
